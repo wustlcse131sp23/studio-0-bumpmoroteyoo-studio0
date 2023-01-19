1a434854bde88f17d91795dd29feaf7026d9b17c
William Yoo <87338673+vvilliamyoo@users.noreply.github.com>
2023-01-19 15:10:30 -0600

    :)

--- src/studio0/BurritoBudget.java
+++ src/studio0/BurritoBudget.java
@@ -7,8 +7,8 @@ public class BurritoBudget {
 		int burritosEatenWeek2 = 4;
 		int burritosEatenWeek3 = 19; //don't judge me
 		int burritosEatenWeek4 = 1;
-		
-		int totalBurritos = burritosEatenWeek1 + burritosEatenWeek2 + burritosEatenWeek3 + burritosEatenWeek4;
+		//switched from "int" to "double" 
+		double totalBurritos = burritosEatenWeek1 + burritosEatenWeek2 + burritosEatenWeek3 + burritosEatenWeek4;
 		double averageWeeklyBurritos = totalBurritos / 4;
 		
 		double burritoPrice = 7.50;
--- src/studio0/MilageReport.java
+++ src/studio0/MilageReport.java
@@ -9,7 +9,8 @@ public class MilageReport {
 		
 		int totalMiles = car1miles + car2miles + car3miles;
 		double milesPerGallon = 24.5;
-		double totalGallons = totalMiles * milesPerGallon;
+		// Switch the operation from multiplying to dividing
+		double totalGallons = totalMiles / milesPerGallon;
 		double pricePerGallon = 3.05;
 		double totalPrice = totalGallons * pricePerGallon;
 		
